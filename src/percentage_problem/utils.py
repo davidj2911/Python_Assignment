5314f58d4cb944cca3783289f9ef138467650670
DavidJacob <david.jacob@diggibyte.com>
2024-05-31 12:59:47 +0530

    made a small change to the utils file and updated it

--- src/percentage_problem/utils.py
+++ src/percentage_problem/utils.py
@@ -1,5 +1,6 @@
 def percentage_cal(stu_marks, name):
     total = sum(stu_marks[name])
-    n = len(stu_marks[name])
+    #n = len(stu_marks[name])
+    n = len(stu_marks)
     avg = format(total/n, '.2f')
     return avg
